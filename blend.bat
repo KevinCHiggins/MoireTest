e2a2dc2a72675740462478250b89ca4c631a4060
Kevin Higgins <kevincolmhiggins@gmail.com>
2020-02-18 17:44:40 +0000

    Revert "Working on order updating, but getting framerate drops"
    This reverts commit 5c4b69192361484d78b6b8a1e8ecaed050a58352.

--- blend.bat
+++ blend.bat
@@ -1,4 +1,4 @@
 
-"C:\Program Files (x86)\Slimjet\slimjet.exe" "http://localhost:8000/blend.html" --new-window
-cd c:\Users\Kevin\web\MoireTest\
+"C:\Program Files (x86)\Slimjet\slimjet.exe" --app="http://localhost:8000/blend.html" 
+cd c:\Kevin\web\MoireTest\
 python -m http.server
